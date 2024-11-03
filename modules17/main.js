b5622f833c202874d0bffa63767980498705ed9c
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-03 09:11:00 +0600

    js first moduels

---
+++ modules17/main.js
@@ -0,0 +1,11 @@
+let sinthiaTk = '15.25';
+
+sinthiaTk = parseFloat(sinthiaTk)
+
+let orangePrice = '20';
+orangePrice = parseInt(orangePrice);
+console.log(typeof sinthiaTk);
+console.log(typeof orangePrice);
+
+const totalTk = sinthiaTk + orangePrice;
+console.log(totalTk);
