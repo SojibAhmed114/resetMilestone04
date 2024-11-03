ca1589584bb815fab7f4a69c3f440dc62e609e2f
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-03 09:51:59 +0600

    math

--- modules17/shorthandOparation.js
+++ modules17/shorthandOparation.js
@@ -1,1 +1,17 @@
-// addition js 
+// Mathematical operation shorthand
+
+let moneytk = 10;
+moneytk += 100;
+console.log(moneytk);
+
+
+let madrasaTk = 145;
+madrasaTk -= 14;
+console.log(madrasaTk);
+
+
+var bazzarTk = 4510;
+bazzarTk *= 2;
+console.log(bazzarTk);
+
+
