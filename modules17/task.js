89b7beb225653bbca75f338adaf42dcee81ebf08
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-04 02:13:36 -0800

    modueles 18 added

---
+++ modules17/task.js
@@ -0,0 +1,40 @@
+// task one 
+let myTotalTk = 7000;
+let orangePrice = 456;
+let mangoPrice = 1421;
+
+let totalCost = orangePrice + mangoPrice;
+const myBlanceNow = myTotalTk - totalCost;
+console.log(myBlanceNow);
+
+
+
+// task Two 
+let mathematics = 51;
+let chemistry = 46;
+let physics = 80;
+let bangla = 52;
+
+let subTotal = mathematics + chemistry + physics + bangla;
+let average = subTotal / 4;
+const total = average.toFixed(1);
+console.log(total);
+
+
+// task Three 
+let myModu = 46;
+let malti = 3;
+
+const moTotal = myModu % malti;
+console.log(moTotal);
+
+
+// task four 
+const a = isNaN('11');
+console.log(a);
+
+const b = isNaN(2 - 10);
+console.log(b);
+
+
+console.log(6 != 6);
---
+++ modules18/main.js
@@ -0,0 +1,14 @@
+var tk = '124.45';
+tk = parseFloat(tk);
+tk += 1000;
+
+
+var tkTwo = '4587';
+tkTwo = parseInt(tkTwo);
+
+let totaltk = tk + tkTwo;
+console.log(totaltk);
+
+
+var x = null;
+console.log(typeof x);
