707e2fbd49175fb24a8ce33f49b5dcce8190f372
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-04 09:24:48 -0800

    logical not try to understant

---
+++ modules18/logical-not.js
@@ -0,0 +1,13 @@
+
+let isPassed = false;
+
+if(!isPassed){
+    console.log('it is  boy');
+}
+else{
+    console.log( 'it is girls');
+}
+
+let isEnable = true;
+isEnable = !isEnable
+console.log(isEnable);
