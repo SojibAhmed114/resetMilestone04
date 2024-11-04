24c080b2df3a37f277f63afb54ba99107a702883
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-04 06:17:50 -0800

    multiple condition js folder done

---
+++ modules18/multiple-condition.js
@@ -0,0 +1,27 @@
+let salary = 4000;
+let isBCS = true;
+let height = 70;
+
+if (salary > 20000 && height < 60){
+    console.log('ami biye korte cai');
+}
+else {
+    console.log('ami biye korbo na vai');
+}
+
+if (salary > 25000 || height > 60 || isBCS === true){
+    console.log('chele valo ache');
+}
+else {
+    console.log('chele ata hobe na vai change kora lagbe');
+    
+}
+
+
+let money = 45000;
+let car = true;
+let bcs = true;
+
+if ((money >50000 && car == true) || bcs == true ){
+    console.log('I have money and car'); 
+}
