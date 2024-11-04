034729ef1977c448c412e33115fb6902976363a9
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-04 05:11:35 -0800

    simple if eles condition

---
+++ modules18/if.js
@@ -0,0 +1,14 @@
+if (4 < 10){
+    console.log('i am a smaller value');
+    
+}
+
+let biriyaniPrice  = 100;
+
+if ( biriyaniPrice < 500){
+    console.log('mama give me some biryani');
+}
+else {
+    console.log('ami khabo na mama');
+    
+}
