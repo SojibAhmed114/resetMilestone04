72a1bfeb6948b300f97b7818f0426482e7c3d608
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-04 07:34:47 -0800

    nested js done

---
+++ modules18/nested.js
@@ -0,0 +1,19 @@
+let torTk = 201;
+
+
+if (torTk >= 300){
+    console.log('kire tor kache to onek tk re');
+}
+else {
+    if (torTk > 200){
+        console.log('ami to voy ar moddhe pore gelam re');
+    }
+    else {
+        if (torTk > 10){
+            console.log('jibon ta asole ki , chele der caite meyera onek sukhi ai life a');
+        }
+        else{
+            console.log('tor kache to tk e nai');
+        }
+    }
+}
