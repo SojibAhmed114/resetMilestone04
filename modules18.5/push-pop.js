b24fa4a00ed093becf8b42c0ca051a53ce0b48ee
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-06 04:20:04 -0800

    push and pop done

---
+++ modules18.5/push-pop.js
@@ -0,0 +1,18 @@
+// array push method
+// push means add new elements to the end of an array, and returns the new length of the array.
+
+let myPushArray = [14,25,36,47,58,69];
+myPushArray.push(500);
+console.log(myPushArray);
+
+
+
+// array pop method
+// pop means removed elements to the end of an array.
+let myPopArray = ['sojib', 'saif', 'sakil'];
+myPopArray.pop();
+console.log(myPopArray);
+// pop kora element alada variable a rakha jay
+let getPopElement = myPopArray.pop();
+console.log(getPopElement);
+
