f2c489f284ecb7489de1d936959aaf1314874df8
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-06 07:08:12 -0800

    ok but task baki

---
+++ modules18.5/join.js
@@ -0,0 +1,9 @@
+//join,, 
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+let checks = fruits.join();
+console.log(checks);
+
+let myCon = [100,200,300,400,500];
+
+let myConcat = fruits.concat(myCon);
+console.log(myConcat);
