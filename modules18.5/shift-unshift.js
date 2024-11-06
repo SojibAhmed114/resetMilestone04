75fd2347f13c1adc62e81831297d4ec9af131eda
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-06 04:27:58 -0800

    shift and unshift done

---
+++ modules18.5/shift-unshift.js
@@ -0,0 +1,14 @@
+// array shift 
+// shift means Removes the first element from an array and returns it
+let myShift = [100,200,300,400,500];
+myShift.shift();
+console.log(myShift);
+
+
+
+
+//array unshift
+// unshift means Inserts new elements at the start of an array
+let arrayUnshift = [45,46,47,48,49,44];
+arrayUnshift.unshift(500);
+console.log(arrayUnshift);
