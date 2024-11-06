f5cf16f0c28d982c0ba4b8df458e6a8278211482
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-06 04:03:51 -0800

    moduels 18.5 length index push pop work

---
+++ modules18.5/array.js
@@ -0,0 +1,19 @@
+// js array
+// let arrayName = ['itemOne', 'itemTwo'];
+
+// array length 
+let myMarks = [14,25,36,58,69,47,48,26,323,353,368,65,4,110,245,256];
+let arrayLength = myMarks.length;
+console.log(arrayLength);
+
+//array get element value by index
+let getElement = [14,25,36,65,68,69,47];
+let getElements =getElement[5];
+console.log(getElements);
+
+
+//array set or update by index
+let setArray = [24,67,48,25,14,29,37,89,97];
+setArray[1] = 'sojib';
+console.log(setArray);
+
