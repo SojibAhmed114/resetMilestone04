34454f41519df35750e3980492784aad6354dbda
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-06 07:29:15 -0800

    task one and two done

---
+++ modules18.5/task.js
@@ -0,0 +1,18 @@
+// task One
+let fruits = ['Apple', 'Papaya', 'Grapefruit', 'Jackfruit', 'mango'];
+
+// find 3rd index element 
+let rd = fruits[3];
+console.log(rd);
+
+// change 2nd index element to jambura
+fruits[2]= 'jambura';
+console.log(fruits);
+
+
+//task Two
+let place = ['Dhaka', 'Gaibandha', 'Cox Bazar'];
+place.push('Pabna');
+place.push('Bogra', 'RAjshahi');
+place.pop();
+console.log(place);
