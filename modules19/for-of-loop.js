5b29b8bfbaa8992e950e96d1b65fbc2dfec6bb8f
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-08 00:47:44 -0800

    while loop

---
+++ modules19/for-of-loop.js
@@ -0,0 +1,17 @@
+// for of loop use into an array.
+let banglaMarks = [15,58,47,58,69,36,25,14,25,36,654,68,69,41];
+console.log(banglaMarks);
+
+for (const singleMarks of banglaMarks){
+    console.log(singleMarks);
+}
+
+//-------------------------
+let fruits = ['apple', 'banana', 'orange', 'jackfruits', 'watermelon'];
+for (const fruit of fruits){
+    console.log(fruit);
+}
+
+
+
+
---
+++ modules19/recap.js
@@ -0,0 +1,14 @@
+let tk = 100;
+
+if (tk >= 100){
+    console.log('buy ticket');
+}
+else {
+    console.log('do not buy ticket');
+}
+
+//! array 
+
+let myArray = [14,25,36,58,69,47,59,48,54,28,29];
+let myArrays = myArray.length
+console.log(myArrays);
