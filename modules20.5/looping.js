ade8c398c6b0605c27f04f5ab10ab85135ed6567
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-11 18:58:58 +0600

    modules 20.5 file added in looping try

---
+++ modules20.5/looping.js
@@ -0,0 +1,20 @@
+const friends = ['emon', 'sanarul', 'milon', 'sujon'];
+for (let friend of friends){
+    // console.log(friend);
+}
+
+for (let i = 0; i < friends.length; i++ ){
+    console.log(friends[i]);
+}
+
+
+const numbers = [14,25,36,58,569,58,47,8,9,262,4245,141,1254];
+for (let n = 0; n < numbers.length; n++){
+    console.log(numbers[n]);
+}
+
+let x = 0;
+while (x < numbers.length){
+    console.log(numbers[x]);
+    x++;
+}
