a78e190f7f214d55a75259c2bcc07597d643c867
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-11 11:48:05 +0600

    parmuna bahe

---
+++ modules20/stringTask.js
@@ -0,0 +1,16 @@
+//----------taskOne ------
+let myStr = 'a dog is a a a a  ';
+let checkA = myStr.split('a').length -1;
+console.log("checkA: ", checkA);
+
+
+//------- task Two ----------
+let myStrTwo = 'a cat is A A A man';
+let checkAllA = myStrTwo.toLowerCase().split('a').length -1;
+console.log(checkAllA);
+
+//----------task Three ------------
+
+
+
+
