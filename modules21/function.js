83dba6a73720951a8f86a2fd04e45fee593a0909
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-16 18:07:18 +0600

    function

---
+++ modules21/function.js
@@ -0,0 +1,29 @@
+//function structure
+// function functionName (paraMeter){}
+
+function myFunction (){
+    console.log('log in kor be');
+}
+
+myFunction();
+
+
+//square 
+
+function square (Number){
+    const result = Number * Number;
+    const resultTwo = Number ** 2;
+
+    console.log("result: ", result);
+    console.log(resultTwo);
+
+}
+square(2);
+
+// addition
+function add (numOne, numTwo){
+    const addition =numOne + numTwo;
+    console.log(addition);
+}
+add(10,10);
+
