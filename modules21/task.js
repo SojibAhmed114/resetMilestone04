50c8fd34a3064e48be37c86a89450c9b7744130b
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-18 22:59:04 +0600

    task one and two done

--- modules21/task.js
+++ modules21/task.js
@@ -1,2 +1,24 @@
 // task One
+function multi (numOne, numTwo, numThree, numFour){
+    const multiple = numOne * numTwo * numThree * numFour;
+    return multiple;
+}
 
+
+const mul = multi(14,25,36,458);
+console.log(mul);
+
+// task Two 
+function double (number){
+    if(number % 2 === 0){
+        const results = number * 2;
+        return results;
+    }
+    else {
+        const results = number * 3;
+        return results;
+    }
+}
+
+let nums = double (3);
+console.log('hi',nums);
