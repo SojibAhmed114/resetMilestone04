46f90c163811af10d47a136a9a158cda979516b0
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-23 21:35:53 +0600

    modules 22 done

---
+++ modules22/date.js
@@ -0,0 +1,10 @@
+// date 
+const today = new Date ();
+console.log(today);
+
+
+const todayTwo = new Date('2078-4-20');
+console.log(todayTwo);
+
+const todayThree = new Date (2078, 2, 20);
+console.log(todayThree.toLocaleString('en-GB'));
