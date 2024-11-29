12e043e1251967246f82245d1b9dcda2e89f8b85
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-11-29 15:53:52 +0600

    simple calculate done

---
+++ modules23/simpleCalculator.js
@@ -0,0 +1,33 @@
+// simple calculator
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+function multiple(num1, num2) {
+    return num1 * num2;
+}
+function division(num1, num2) {
+    return num1 / num2;
+}
+
+function calculator(x, y, calculation) {
+    if (calculation === 'add') {
+        const result = add(x, y);
+        return result;
+    }
+    else if (calculation === 'subtract') {
+        const result = subtract(x, y);
+        return result;
+    }
+    else if (calculation === 'multiple') {
+        return multiple(x, y);
+    }
+    else if (calculation === "division") {
+        return division(x,y);
+    }
+}
+
+console.log(calculator(4,4, 'multiple'));
